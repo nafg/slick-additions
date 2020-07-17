2cb30a5121bc9cf01158c2d1ec2f5ded461725d1
Naftoli Gugenheim <98384+nafg@users.noreply.github.com>
2020-07-17 00:16:00 -0400

    entity: EntityRef#updated

--- slick-additions-entity/src/main/scala/slick/additions/entity/Entity.scala
+++ slick-additions-entity/src/main/scala/slick/additions/entity/Entity.scala
@@ -3,12 +3,14 @@ package slick.additions.entity
 
 sealed trait EntityRef[K, +A] {
   def transform[B](f: A => B): EntityRef[K, B]
+  def updated[B](value: B): EntityRef[K, B]
   def widen[B >: A]: EntityRef[K, B]
 }
 
 sealed trait Lookup[K, +A] extends EntityRef[K, A] {
   def key: K
   override def transform[B](f: A => B): Lookup[K, B]
+  override def updated[B](value: B): Lookup[K, B]
   override def widen[B >: A]: Lookup[K, B]
   def foldLookup[X](key: EntityKey[K, A] => X, ent: KeyedEntity[K, A] => X): X = this match {
     case ek: EntityKey[K, A]   => key(ek)
@@ -19,8 +21,9 @@ sealed trait Lookup[K, +A] extends EntityRef[K, A] {
   def toEntityKey: EntityKey[K, A]
 }
 
-case class EntityKey[K, +A](key: K) extends Lookup[K, A] {
+case class EntityKey[K, +A](override val key: K) extends Lookup[K, A] {
   override def transform[B](f: A => B): EntityKey[K, B] = copy()
+  override def updated[B](value: B) = ModifiedEntity(key, value)
   override def widen[B >: A]: EntityKey[K, B] = transform(identity)
   override def toEntityKey = this
 }
@@ -33,6 +36,7 @@ sealed trait Entity[K, +A] extends EntityRef[K, A] {
   def isSaved: Boolean
 
   override def transform[B](f: A => B): Entity[K, B]
+  override def updated[B](value: B): Entity[K, B]
   override def widen[B >: A]: Entity[K, B]
   def modify[B](f: A => B): Entity[K, B]
 
@@ -46,14 +50,15 @@ sealed trait Entity[K, +A] extends EntityRef[K, A] {
     case ke: KeyedEntity[K, A]   => keyed(ke)
   }
 }
-case class KeylessEntity[K, +A](value: A) extends Entity[K, A] {
+case class KeylessEntity[K, +A](override val value: A) extends Entity[K, A] {
   override val keyOption = None
 
   final override def isSaved = false
 
   override def equals(that: Any) = this eq that.asInstanceOf[AnyRef]
 
-  override def transform[B](f: A => B): KeylessEntity[K, B] = new KeylessEntity[K, B](f(value))
+  override def transform[B](f: A => B): KeylessEntity[K, B] = copy(value = f(value))
+  override def updated[B](value: B): KeylessEntity[K, B] = copy(value = value)
   override def widen[B >: A]: KeylessEntity[K, B] = transform(identity)
   override def modify[B](f: A => B) = transform(f)
   final override def map[B >: A](f: A => B): KeylessEntity[K, B] = modify(f)
@@ -67,6 +72,7 @@ sealed trait KeyedEntity[K, +A] extends Entity[K, A] with Lookup[K, A] {
   override def transform[B](f: A => B): KeyedEntity[K, B]
   override def widen[B >: A]: KeyedEntity[K, B]
   override def modify[B](f: A => B): ModifiedEntity[K, B] = ModifiedEntity[K, B](key, f(value))
+  override def updated[B](value: B): ModifiedEntity[K, B] = ModifiedEntity(key, value)
   final override def map[B >: A](f: A => B): ModifiedEntity[K, B] = modify(f)
 
   def toSaved: SavedEntity[K, A] = SavedEntity(key, value)
@@ -77,13 +83,13 @@ object KeyedEntity {
   def apply[K, A](key: K, value: A): KeyedEntity[K, A] = SavedEntity[K, A](key, value)
   def unapply[K, A](ke: KeyedEntity[K, A]): Option[(K, A)] = Some((ke.key, ke.value))
 }
-case class SavedEntity[K, +A](key: K, value: A) extends KeyedEntity[K, A] {
+case class SavedEntity[K, +A](override val key: K, override val value: A) extends KeyedEntity[K, A] {
   final override def isSaved = true
-  override def transform[B](f: A => B): SavedEntity[K, B] = SavedEntity(key, f(value))
+  override def transform[B](f: A => B): SavedEntity[K, B] = copy(value = f(value))
   override def widen[B >: A]: SavedEntity[K, B] = transform(identity)
 }
-case class ModifiedEntity[K, +A](key: K, value: A) extends KeyedEntity[K, A] {
+case class ModifiedEntity[K, +A](override val key: K, override val value: A) extends KeyedEntity[K, A] {
   final override def isSaved = false
-  override def transform[B](f: A => B): ModifiedEntity[K, B] = ModifiedEntity(key, f(value))
+  override def transform[B](f: A => B): ModifiedEntity[K, B] = copy(value = f(value))
   override def widen[B >: A]: ModifiedEntity[K, B] = transform(identity)
 }
