06ab023d30ee69519341bc5ddf62f57389214a11
Scala Steward <scala_steward@virtuslab.com>
2026-01-17 19:32:36 +0000

    Reformat with scalafmt 3.10.4
    Executed command: scalafmt --non-interactive

--- src/test/scala/slick/additions/KeyedTableTests.scala
+++ src/test/scala/slick/additions/KeyedTableTests.scala
@@ -45,11 +45,12 @@ class KeyedTableTests extends AnyFunSuite with Matchers with TestsCommon with In
   }
 
   test("Using lookup in a query") {
-    val res = db run (for {
-      id <- People.map(_.mapping) returning People.map(_.lookup) += Person("first1", "last1")
-      _  <- Phones.map(_.mapping) += Phone("M", "111", Some(id))
-      xs <- People.filter(_.lookup in Phones.map(_.person)).result
-    } yield xs)
+    val res = db run
+      (for {
+        id <- People.map(_.mapping) returning People.map(_.lookup) += Person("first1", "last1")
+        _  <- Phones.map(_.mapping) += Phone("M", "111", Some(id))
+        xs <- People.filter(_.lookup in Phones.map(_.person)).result
+      } yield xs)
     res.futureValue
   }
 
@@ -63,11 +64,12 @@ class KeyedTableTests extends AnyFunSuite with Matchers with TestsCommon with In
 
   test("Using lookup in a predicate doesn't pollute the * projection") {
     // noinspection SimplifyBoolean
-    val res = db run (for {
-      _  <- People.map(_.mapping) += Person("first", "last")
-      cs <- People.filter(people => people.lookup === EntityKey[Long, Person](1).asLookup || true).result.head
-      k   = cs.key
-    } yield k)
+    val res = db run
+      (for {
+        _  <- People.map(_.mapping) += Person("first", "last")
+        cs <- People.filter(people => people.lookup === EntityKey[Long, Person](1).asLookup || true).result.head
+        k   = cs.key
+      } yield k)
     assert(res.futureValue.isInstanceOf[Long])
   }
 }
